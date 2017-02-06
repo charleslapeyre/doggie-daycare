9bc84cdb74aec89f34a93490b189bd980f0342ce
charleslapeyre <lapeyrecc@gmail.com>
2017-02-06 08:37:07 -0600

    Add better errors update gems to include better error messages

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,27 @@
+class DogsController < ApplicationController
+  def index
+    # asking the Dog class in the model to return all of the dogs
+    @dogs = Dog.all
+  end
+  def new
+    @dog = Dog.new
+  end
+  def create
+    @dog = Dog.new(dog_params)
+
+    if @dog.save
+      redirect_to dogs_url, notice: "Successfully Saved"
+    else
+      flash.now[:notice] = "Something went wrong and we couldn't save your dog."
+      render :new
+    end
+
+  end
+
+  private
+  # protection from SQL injection using strong params
+  def dog_params
+    params.require(:dog).permit(:name, :age, :breed)
+  end
+
+end
