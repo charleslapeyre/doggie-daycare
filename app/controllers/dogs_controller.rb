a46cd196a8de89a07affaf29fdad77cf4c08eea1
charleslapeyre <lapeyrecc@gmail.com>
2017-02-06 09:11:37 -0600

    added a dog profile page

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,20 +3,25 @@ class DogsController < ApplicationController
     # asking the Dog class in the model to return all of the dogs
     @dogs = Dog.all
   end
+
   def new
     @dog = Dog.new
   end
+
   def create
     @dog = Dog.new(dog_params)
-
     if @dog.save
       redirect_to dogs_url, notice: "Successfully Saved"
     else
       flash.now[:notice] = "Something went wrong and we couldn't save your dog."
       render :new
     end
-
   end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
 
   private
   # protection from SQL injection using strong params
